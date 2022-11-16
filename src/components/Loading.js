8a1d5540b99ac0f107069dab0a62521c75b4a8a4
Bram <brampijper@gmail.com>
2022-11-16 16:28:23 +0100

    Any error during fetch will display on screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Loading() {
+export default function Loading(props) {
     return (
-        <div>generating questions...</div>
+        <h3>{props.text}</h3>
     )
 }
